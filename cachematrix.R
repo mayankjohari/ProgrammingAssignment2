fb0ea975a18190568269701e5e79e8535fc69ab5
Mayank Johari <mayank.johari@gmail.com>
2014-11-23 22:58:32 +0530

    R Programming - Assignment2 : mayank.johari@gmail.com

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
+## @author Mayank Johari (mayank.johari@gmail.com)
+## Feature to store the inverse of the matrix in the cache memory of runtime envrionment. 
 ## functions do
 
-## Write a short comment describing this function
+## Function 1 (makeCacheMatrix) : Create a matrix chache in the memory and also responisble to poupulate it.
+## This function also define the supporting functions to access the modify the cache.
+
+## Usage:: cacheSolve(makeCacheMatrix(<matrix_for_inversion>))
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix <- function(solve) m<<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,setmatrix=setmatrix,getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## Function 2 (cacheSolve) : Logic to calculate the inverse of the square matrix using "solve"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Function return a matrix that is the inverse of 'x'
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
